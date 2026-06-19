fa3529ae1490d077df662cecbff94129dcaa2a63
ChauhanVinay <vc0627344@gmail.com>
2026-06-19 12:01:08 +0530

    Adding new movie in to list

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { Container, Button, Table, Spinner, Alert } from 'react-bootstrap';
+import { Container, Button, Table, Spinner, Alert, Form } from 'react-bootstrap';
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
@@ -9,6 +9,11 @@ export default function Movies() {
   
   // useRef keeps track of the active timer interval so we can clear it anywhere
   const retryIntervalRef = useRef(null);
+
+  //Form refs
+  const titleRef = useRef('');
+  const openingTextRef = useRef('');
+  const releaseDateRef = useRef('');
 
   // The main async/await movie fetching function
   const  fetchMoviesHandler = useCallback(async () => {
@@ -62,9 +67,45 @@ useEffect(() => {
     }
   }
 
+  //Form Submit Handler
+  function submitHandler(event) {
+    event.preventDefault();
+    const NewMoviesObj = {
+      title: titleRef.current.value,
+      openingText: openingTextRef.current.value,
+      releaseDate: releaseDateRef.current.value,
+    };
+    console.log(NewMoviesObj);
+
+    //clear Inputs
+    titleRef.current.value = '';
+    openingTextRef.current.value = '';
+    releaseDateRef.current.value = '';
+  }
 
   return (
     <Container className="py-5 text-center" style={{ maxWidth: '800px' }}>
+ 
+      {/* ADD MOVIE FORM */}
+      <Form onSubmit={submitHandler} className="p-4 border rounded shadow-sm bg-light mb-5">
+      <Form.Group className="mb-3">
+  <Form.Label className="fw-bold">Title</Form.Label>
+  <Form.Control type="text" ref={titleRef} required/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+          <Form.Label className="fw-bold">Opening Text</Form.Label>
+          <Form.Control as="textarea" rows={3} ref={openingTextRef} required />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label className="fw-bold">Release Date</Form.Label>
+          <Form.Control type="date" ref={releaseDateRef} required />
+        </Form.Group>
+  <div className="d-grid">
+    <Button type="submit" variant="primary" className="fw-bold">Add Movie</Button>
+  </div>
+      </Form>
+
+    <div className="text-center">
       <h2 className="mb-4 fw-bold font-monospace">STAR WARS MOVIES</h2>
       
       <div className="mb-4">
@@ -121,6 +162,7 @@ useEffect(() => {
           </tbody>
         </Table>
       )}
+      </div>
     </Container>
   );
 }
